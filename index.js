a8633f39975d722ca2522abe116f01496ae3eebe
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2023-09-17 11:11:15 +0530

    use to colors to paint

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.container');
 
+// produce grid
 for (let i = 0; i < 16; i++) {
   const row = document.createElement('div');
 
@@ -15,8 +16,23 @@ for (let i = 0; i < 16; i++) {
   container.appendChild(row);
 }
 
+
+const rainbowColors = [
+  '#FF0000', // Red
+  '#FF7F00', // Orange
+  '#FFFF00', // Yellow
+  '#00FF00', // Green
+  '#0000FF', // Blue
+  '#4B0082', // Indigo
+  '#9400D3'  // Violet
+];
+
+let colorChooser = 0;
 function paint(e) {
-  e.target.style.backgroundColor = 'black';
+  let color = colorChooser % 7;
+  e.target.style.backgroundColor = rainbowColors[color];
+
+  colorChooser++;
 }
 const squares = document.querySelectorAll('.column');
 
