c7057989f0fc94bd9570fd33e80a9a2a5f96fdbe
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2023-09-17 15:00:21 +0530

    add black , rainbow ,eraser option

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const container = document.querySelector('.container');
 
+
+let colorChooser = 0;
+
+let Colors = [
+  '#FF0000', // Red
+  '#FF7F00', // Orange
+  '#FFFF00', // Yellow
+  '#00FF00', // Green
+  '#0000FF', // Blue
+  '#4B0082', // Indigo
+  '#9400D3'  // Violet
+];
+
+
+function paintRainbow(e) {
+  let color = colorChooser % 7;
+  e.target.style.backgroundColor = Colors[color];
+
+  colorChooser++;
+}
+
 // produce grid
 for (let i = 0; i < 16; i++) {
   const row = document.createElement('div');
@@ -17,28 +38,42 @@ for (let i = 0; i < 16; i++) {
 }
 
 
-const rainbowColors = [
-  '#FF0000', // Red
-  '#FF7F00', // Orange
-  '#FFFF00', // Yellow
-  '#00FF00', // Green
-  '#0000FF', // Blue
-  '#4B0082', // Indigo
-  '#9400D3'  // Violet
-];
 
-let colorChooser = 0;
-function paint(e) {
-  let color = colorChooser % 7;
-  e.target.style.backgroundColor = rainbowColors[color];
-
-  colorChooser++;
-}
 const squares = document.querySelectorAll('.column');
 
+
+
+
+const black = document.querySelector('button.black');
+
+const rainbow = document.querySelector('button.rainbow');
+
+const eraser = document.querySelector('button.eraser');
+
+
+black.addEventListener('click', () => {
+
+  Colors = ['#000', '#000', '#000', '#000', '#000', '#000', '#000', '#000'];
+});
+
+rainbow.addEventListener('click', () => {
+
+  Colors = [
+    '#FF0000', // Red
+    '#FF7F00', // Orange
+    '#FFFF00', // Yellow
+    '#00FF00', // Green
+    '#0000FF', // Blue
+    '#4B0082', // Indigo
+    '#9400D3'  // Violet
+  ];
+});
+
+eraser.addEventListener('click', () => {
+
+  Colors = ['#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff'];
+});
 squares.forEach((square) => {
 
-
-
-  square.addEventListener('mouseover', paint)
+  square.addEventListener('mouseover', paintRainbow);
 });
